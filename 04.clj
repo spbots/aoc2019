3c830f80be1794d1417d42a4fe050552f8338a9f
spbots <stephan+github@stephanbots.com>
2019-12-05 23:15:47 +0100

    AOC 2019 Day 4 part 2

--- 04.clj
+++ 04.clj
@@ -4,35 +4,41 @@
 (def MINVAL 123257)
 (def MAXVAL 647015)
 
-; i can't believe i need to define this...
-; (defn exp [base e] (reduce * (repeat e base)))
-
 ; https://stackoverflow.com/a/22772070/101645
 (defn digits [x]
     (if (< x 10)
         [x]
         (conj (digits (quot x 10)) (rem x 10))))
-(println (digits 123))
-(println (digits 523783))
 
 (defn increasing-digits? [x]
     (>= (reduce (fn [a b] (if (and
         (>= b a) (>= a 0)) b -1)) 0 x) 0))
 
-(defn has-double? [x]
-    (= (reduce (fn [a b] (if (or (= b a) (= a -1)) -1 b)) 0 x) -1))
+(defn collapse-digits [x]
+    (reduce
+        (fn [acc x]
+            (def y (peek acc))
+            (if (and y (= x (rem y 10)))
+                (conj (pop acc) (+ x (* y 10)))
+                (conj acc x)
+            )) [] x))
 
-(println (has-double? [1 2 3 3 4 5]))
-(println (increasing-digits? [1 2 3 4 5]))
+(defn correct-doubles? [d]
+    (reduce
+    				; part 1:
+        ; (fn [acc x] (if (> x 10) true acc))
+        ; part 2:
+        (fn [acc x] (if (and (> x 10) (< x 100)) true acc))
+        false
+        (collapse-digits d)))
 
 (defn is-valid? [x]
     (def d (digits x))
     (and
         (increasing-digits? d)
-        (has-double? d)))
+        (correct-doubles? d)))
 
 (println (reduce
-    (fn [sum n]
-        (if (is-valid? n) (+ 1 sum) sum))
+    (fn [sum n] (if (is-valid? n) (+ 1 sum) sum))
     0
     (range MINVAL MAXVAL)))
