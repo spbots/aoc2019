0fe4cd3c706fd6459b0a54b4c774ee271c0be448
spbots <stephan+github@stephanbots.com>
2019-12-06 00:01:19 +0100

    Refactor day 2
    change the opcode computing so that it's responsible for incrementing
    the index. the index is stored temporarily at the end of the vector.

--- 02.clj
+++ 02.clj
@@ -1,32 +1,35 @@
 (require '[clojure.string :as str])
 
 (defn parse-int [s] (Integer. (re-find  #"\d+" s)))
-(def file-in (mapv identity (map parse-int (str/split (slurp "./input/02") #","))))
+(def file-in (vec (map parse-int (str/split (slurp "./input/02") #","))))
 
-(defn parse-opcode [arr pos]
+(defn parse-opcode [a]
+    (def pos (peek a))
+    (def arr (pop a))
     (def opcode (nth arr pos))
     (def arg1 (nth arr (nth arr (+ pos 1))))
     (def arg2 (nth arr (nth arr (+ pos 2))))
     (def store-at (nth arr (+ pos 3)))
     (case opcode
-        1 (assoc arr store-at (+ arg1 arg2))
-        2 (assoc arr store-at (* arg1 arg2))))
+        1 (conj (assoc arr store-at (+ arg1 arg2)) (+ 4 pos))
+        2 (conj (assoc arr store-at (* arg1 arg2)) (+ 4 pos))))
 
-; return
-(defn get-return-code [arr]
-    (loop [arr arr idx 0]
+(defn run-program [arr]
+    ; use the last element as the instruction pointer
+    (loop [arr (conj arr 0)]
+        (def idx (peek arr))
         (if (= 99 (nth arr idx))
-            (nth arr 0)
-            (recur (parse-opcode arr idx) (+ 4 idx)))))
+            (pop arr)
+            (recur (parse-opcode arr)))))
 
 (defn set-noun-verb [n v] (assoc (assoc file-in 1 n) 2 v))
 
 ; replace pos 1 -> 12, 2 -> 2
-(println (get-return-code (set-noun-verb 12 2)))
+(println (nth (run-program (set-noun-verb 12 2)) 0))
 
 (defn print-if-magic [noun verb]
     (def magic-number 19690720)
-    (if (= magic-number (get-return-code (set-noun-verb noun verb)))
+    (if (= magic-number (nth (run-program (set-noun-verb noun verb)) 0))
         (println (+ verb (* 100 noun)))))
 
 (filterv
