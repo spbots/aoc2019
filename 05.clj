5e4ba9cf0398ea660a45426710d1e83669c50dee
spbots <stephan+github@stephanbots.com>
2019-12-06 11:57:19 +0100

    AOC 2019 Day 5 part 2

--- 05.clj
+++ 05.clj
@@ -15,33 +15,45 @@
      (rem (quot opcode 10000) 10)])
 
 (defn parse-opcode [a input]
-    ; (println a)
     (def pos (peek a))
     (def arr (pop a))
     (def opcode (nth arr pos))
     (def modes (arg-modes opcode))
+    (defn op-arg [n] (get-arg arr (+ pos n 1) (nth modes n)))
+    (defn store-addr [n] (nth arr (+ pos n 1)))
+
     (case (rem opcode 100) ; 2 digit opcode
-        1 (conj (assoc arr (nth arr (+ pos 3))
-            (+ (get-arg arr (+ pos 1) (nth modes 0))
-               (get-arg arr (+ pos 2) (nth modes 1))))
+        ; add a b store
+        1 (conj
+            (assoc arr (store-addr 2) (+ (op-arg 0) (op-arg 1)))
             (+ 4 pos))
-        2 (conj (assoc arr (nth arr (+ pos 3))
-            (* (get-arg arr (+ pos 1) (nth modes 0))
-               (get-arg arr (+ pos 2) (nth modes 1))))
+        ; mult a b store
+        2 (conj
+            (assoc arr (store-addr 2) (* (op-arg 0) (op-arg 1)))
             (+ 4 pos))
-        3 (do
-            (println "IN")
-            (conj (assoc arr (nth arr (+ pos 1)) input) (+ 2 pos)))
-        4 (do
-            (println "OUT:" (get-arg arr (+ pos 1) (nth modes 0)))
+        ; input store
+        3 (conj (assoc arr (store-addr 0) input) (+ 2 pos))
+        ; output store
+        4 (do (println "OUT:" (op-arg 0))
             (conj arr (+ 2 pos)))
+        ; jump-true condition instruction-pointer
+        5 (conj arr
+            (if (not (= 0 (op-arg 0))) (op-arg 1) (+ 3 pos)))
+        ; jump-false condition instruction-pointer
+        6 (conj arr
+            (if (= 0 (op-arg 0)) (op-arg 1) (+ 3 pos)))
+        ; lt a b store
+        7 (conj
+            (assoc arr (store-addr 2) (if (< (op-arg 0) (op-arg 1)) 1 0))
+            (+ 4 pos))
+        ; eq a b store
+        8 (conj
+            (assoc arr (store-addr 2) (if (= (op-arg 0) (op-arg 1)) 1 0))
+            (+ 4 pos))
+        ; end
         99 (conj arr -1)
         ))
 
-; (println (parse-opcode `[1001 4 3 4 33 0] 1))
-; (println (parse-opcode `[11001 4 3 4 33 0] 1))
-; (println (parse-opcode `[1101 4 3 4 33 0] 1))
-; (println (parse-opcode `[101 4 3 4 33 0] 1))
 
 (defn run-program [arr input]
     ; use the last element as the instruction pointer
@@ -50,5 +62,5 @@
             (pop arr)
             (recur (parse-opcode arr input)))))
 
-(def INPUT 1)
+(def INPUT 5)
 (run-program file-in INPUT)
