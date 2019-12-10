0f5adef6c30f3d1ef3a1735ba22e93dfbbb6f9b5
spbots <stephan+github@stephanbots.com>
2019-12-10 12:26:32 +0100

    Use shared intcode computer for 5 & 7

--- 05.clj
+++ 05.clj
@@ -1,66 +1,15 @@
-(require '[clojure.string :as str])
+(require '(clojure [string :as str] [test :as test]))
+(load-file "./intcode.clj")
+(require '[aoc.intcode :as intcode])
 
 (defn parse-int [s] (Integer. (re-find  #"-?\d+" s)))
 (def file-in (vec (map parse-int (str/split (slurp "./input/05") #","))))
 
-(defn get-arg [arr pos mode]
-    (case mode
-        0 (nth arr (nth arr pos)) ; position
-        1 (nth arr pos))) ; immediate
+(defn diagnostic-out [i]
+    (peek ((intcode/run-program (intcode/init-state file-in [i])) :o)))
 
-(defn arg-modes [opcode]
-    ; this can probably be a reduce, but don't worry about it for now.
-    [(rem (quot opcode 100) 10)
-     (rem (quot opcode 1000) 10)
-     (rem (quot opcode 10000) 10)])
+(println (diagnostic-out 1))
+(test/is (= 15426686 (diagnostic-out 1)))
 
-(defn parse-opcode [a input]
-    (def pos (peek a))
-    (def arr (pop a))
-    (def opcode (nth arr pos))
-    (def modes (arg-modes opcode))
-    (defn op-arg [n] (get-arg arr (+ pos n 1) (nth modes n)))
-    (defn store-addr [n] (nth arr (+ pos n 1)))
-
-    (case (rem opcode 100) ; 2 digit opcode
-        ; add a b store
-        1 (conj
-            (assoc arr (store-addr 2) (+ (op-arg 0) (op-arg 1)))
-            (+ 4 pos))
-        ; mult a b store
-        2 (conj
-            (assoc arr (store-addr 2) (* (op-arg 0) (op-arg 1)))
-            (+ 4 pos))
-        ; input store
-        3 (conj (assoc arr (store-addr 0) input) (+ 2 pos))
-        ; output store
-        4 (do (println "OUT:" (op-arg 0))
-            (conj arr (+ 2 pos)))
-        ; jump-true condition instruction-pointer
-        5 (conj arr
-            (if (not (= 0 (op-arg 0))) (op-arg 1) (+ 3 pos)))
-        ; jump-false condition instruction-pointer
-        6 (conj arr
-            (if (= 0 (op-arg 0)) (op-arg 1) (+ 3 pos)))
-        ; lt a b store
-        7 (conj
-            (assoc arr (store-addr 2) (if (< (op-arg 0) (op-arg 1)) 1 0))
-            (+ 4 pos))
-        ; eq a b store
-        8 (conj
-            (assoc arr (store-addr 2) (if (= (op-arg 0) (op-arg 1)) 1 0))
-            (+ 4 pos))
-        ; end
-        99 (conj arr -1)
-        ))
-
-
-(defn run-program [arr input]
-    ; use the last element as the instruction pointer
-    (loop [arr (conj arr 0)]
-        (if (= -1 (peek arr))
-            (pop arr)
-            (recur (parse-opcode arr input)))))
-
-(def INPUT 5)
-(run-program file-in INPUT)
+(println (diagnostic-out 5))
+(test/is (= 11430197 (diagnostic-out 5)))
--- 07.clj
+++ 07.clj
@@ -1,87 +1,18 @@
-(require '[clojure.string :as str])
+(require '(clojure [string :as str] [test :as test]))
+(load-file "./intcode.clj")
+(require '[aoc.intcode :as intcode])
 
 (defn parse-int [s] (Integer. (re-find  #"-?\d+" s)))
 (def file-in (vec (map parse-int (str/split (slurp "./input/07") #","))))
-(def debug-in1 (vec (map parse-int (str/split "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0" #","))))
-(def debug-in2 (vec (map parse-int (str/split "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0" #","))))
-(def debug-in3 (vec (map parse-int (str/split "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0" #","))))
 
-(defn init-state [mem inputs] {
-    :m mem
-    :i inputs
-    :o []   ; outputs
-    :iptr 0 ; instruction pointer
-    })
-
-(defn get-arg [arr pos mode]
-    (case mode
-        0 (nth arr (nth arr pos)) ; position
-        1 (nth arr pos))) ; immediate
-
-(defn arg-modes [opcode]
-    ; this can probably be a reduce, but don't worry about it for now.
-    [(rem (quot opcode 100) 10)
-     (rem (quot opcode 1000) 10)
-     (rem (quot opcode 10000) 10)])
-
-(defn parse-opcode [pgm]
-    (def arr (pgm :m))
-    (def pos (pgm :iptr))
-    (def opcode (nth (pgm :m) pos))
-    (def modes (arg-modes opcode))
-    (defn op-arg [n] (get-arg arr (+ pos n 1) (nth modes n)))
-    (defn store-addr [n] (nth arr (+ pos n 1)))
-
-    (defn update-mem [p store-addr x]
-        (conj p {:m (assoc (pgm :m) store-addr x)}))
-    (defn inc-iptr [n p] (conj p {:iptr (+ n (p :iptr))}))
-
-    (case (rem opcode 100) ; 2 digit opcode
-        ; add a b store
-        1 (inc-iptr 4 (update-mem pgm (store-addr 2) (+ (op-arg 0) (op-arg 1))))
-        ; mult a b store
-        2 (inc-iptr 4 (update-mem pgm (store-addr 2) (* (op-arg 0) (op-arg 1))))
-        ; input store
-        3 (do (def inval (peek (pgm :i)))
-          (inc-iptr 2 (update-mem (conj pgm {:i (pop (pgm :i))}) (store-addr 0) inval)))
-        ; output store
-        4 (do ;(println "OUT:" (op-arg 0))
-            (inc-iptr 2 (conj pgm {:o (conj (pgm :o) (op-arg 0))})))
-        ; jump-true condition instruction-pointer
-        5 (conj pgm
-            {:iptr (if (not (= 0 (op-arg 0))) (op-arg 1) (+ 3 pos))})
-        ; jump-false condition instruction-pointer
-        6 (conj pgm
-            {:iptr (if (= 0 (op-arg 0)) (op-arg 1) (+ 3 pos))})
-        ; lt a b store
-        7 (inc-iptr 4 (update-mem pgm (store-addr 2) (if (< (op-arg 0) (op-arg 1)) 1 0)))
-        ; eq a b store
-        8 (inc-iptr 4 (update-mem pgm (store-addr 2) (if (= (op-arg 0) (op-arg 1)) 1 0)))
-        ; end (only updates iptr)
-        99 (conj pgm {:iptr -1})))
-
-
-(defn run-program [program]
-    ; use the last element as the instruction pointer
-    ; end-1 is the size of inputs, then the inputs.
-    ; [...program...    12 3 2 (inputs & size)     0 (iptr)]
-    (loop [pgm program]
-        (if (= -1 (pgm :iptr))
-            pgm
-            (recur (parse-opcode pgm)))))
-
-(defn amp-e-output [pgm phases] (reduce
+(defn amp-e-output [mem phases] (reduce
     (fn [acc x]
-        (def result (run-program (init-state pgm [acc x])))
+        (def result (intcode/run-program (intcode/init-state mem [acc x])))
         (nth (result :o) 0))
     0
     phases))
 
-; (println (amp-e-output debug-in1 [4 3 2 1 0]))
-; (println (amp-e-output debug-in2 [0 1 2 3 4]))
-; (println (amp-e-output debug-in3 [1 0 4 3 2]))
-
-(println (apply max (for [phA (range 5)
+(def part1 (apply max (for [phA (range 5)
                           phB (range 5)
                           phC (range 5)
                           phD (range 5)
@@ -90,6 +21,15 @@
         (amp-e-output file-in [phA phB phC phD phE])
         0
         ))))
+(println part1)
+(test/is (= 51679 part1))
 
-; (def INPUT 5)
-; (println "NEWOUT:" ((run-program (init-state file-in [INPUT])) :o))
+(defn amp-e-feedback-out [mem phases]
+    (def start-states (map #(intcode/init-state mem %) phases)) 
+    (println start-states)
+)
+
+(def debug-in (vec (map parse-int (str/split "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5" #","))))
+(def debug-max 139629729)
+(println (amp-e-feedback-out debug-in [9 8 7 6 5]))
+(test/is (= debug-max (amp-e-feedback-out debug-in [9 8 7 6 5])))
