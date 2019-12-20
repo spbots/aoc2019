b5f74f79a13bbe109eb6ab355a21ee7aeb79c2d5
spbots <stephan+github@stephanbots.com>
2019-12-20 09:16:55 -0700

    AOC 2019 Day 14 part 1

---
+++ 14.clj
@@ -0,0 +1,98 @@
+(require '(clojure [string :as str] [test :as test]))
+(load-file "./intcode.clj")
+(require '[aoc.intcode :as intcode])
+
+(def debug-in (str
+"9 ORE => 2 A\n"
+"8 ORE => 3 B\n"
+"7 ORE => 5 C\n"
+"3 A, 4 B => 1 AB\n"
+"5 B, 7 C => 1 BC\n"
+"4 C, 1 A => 1 CA\n"
+"2 AB, 3 BC, 4 CA => 1 FUEL\n"))
+
+; plan:
+; split the line at the =>
+;   - use the second part as a number and keyword (e.g. 1 :FUEL)
+;   - parse the first part as a collection of the same (3 :A 4 :B)
+; store in map {:FUEL {:out 1 :in {:A 3 :B 4} }}
+; add info to the map: number of reactions to get from ORE to :chem
+; starting at fuel, replace the chemicals with the largest number of
+;   reactions and replacing chems at the maximum level.
+; e.g. annotating the next input with distances for the sample input
+;   shows that FUEL is d3, meaning that we need to first replace all
+;   the d2 ingredients, then d1 ingredients.
+(def debug-in2 (str ; 13312 ORE for 1 FUEL
+"157 ORE => 5 NZVS\n" ; d 1
+"165 ORE => 6 DCFZ\n" ; d 1
+"44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL\n" ; d 3
+"12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ\n" ; d 2
+"179 ORE => 7 PSHF\n" ; d 1
+"177 ORE => 5 HKGWZ\n" ; d 1
+"7 DCFZ, 7 PSHF => 2 XJWVT\n" ; d 2
+"165 ORE => 2 GPVTF\n" ; d 1
+"3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT\n")) ; d 2
+
+(defn parse-int [s] (Integer. (re-find  #"-?\d+" s)))
+(defn str-to-chem [s] {(keyword (nth (str/split s #" ") 1)) (parse-int (nth (str/split s #" ") 0))})
+(defn input-to-dict [s]
+    (reduce (fn [acc x]
+        (def in-and-out (filter
+            (fn [y] (< 0 (count y)))
+            (map str/trim (str/split x #"[=>]"))))
+        (def out (str-to-chem (nth in-and-out 1)))
+        (def inputs (reduce
+            (fn [acc x] (conj acc (str-to-chem x)))
+            {}
+            (map str/trim (str/split (nth in-and-out 0) #","))))
+        (conj acc {(apply key out) {:n (apply val out) :in inputs}}))
+    {}
+    (str/split s #"\n")))
+
+(defn distance-from-ore [dict chem]
+    (cond
+        (not (dict chem)) 0 ; not in dict, this is probably :ORE
+        ((dict chem) :d) ((dict chem) :d) ; existing distance
+        :else (+ 1 (apply max (map
+            #(distance-from-ore dict (get % 0))
+            ((dict chem) :in))))))
+
+(defn add-distances-from-ore [dict]
+    (reduce (fn [acc x]
+        (def chem-name (get x 0))
+        (def chem-details (get x 1))
+        (assoc acc chem-name (assoc chem-details :d (distance-from-ore dict chem-name))))
+    dict
+    dict))
+
+(def dbg-init-state (add-distances-from-ore (input-to-dict debug-in2)))
+(def init-state (add-distances-from-ore (input-to-dict (slurp "./input/14"))))
+
+(test/is (= 1 ((dbg-init-state :PSHF) :d)))
+(test/is (= 2 ((dbg-init-state :XJWVT) :d)))
+(test/is (= 3 ((dbg-init-state :FUEL) :d)))
+
+(defn reduce-to-ore [dict chem n]
+    (defn max-dist [inputs] (reduce (fn [acc x] (max acc (distance-from-ore dict (key x)))) 0 inputs))
+    (defn inputs-for [chem n]
+        (def n-produced ((dict chem) :n))
+        (def times-to-run-reaction (+ (quot n n-produced) (if (= 0 (mod n n-produced)) 0 1)))
+        (defn scale-amount-by [n]
+            (fn [acc x] (assoc acc (get x 0) (* n (get x 1)))))
+        (reduce (scale-amount-by times-to-run-reaction) {} ((dict chem) :in)))
+
+    ; substitute all the chemicals that are at the max distance until distance 0
+    (loop [inputs (inputs-for chem n)]
+        (if (= 1 (count inputs))
+            inputs
+            (do
+                (def d (max-dist inputs))
+                (def in-remaining (filter #(< (distance-from-ore dict (key %)) d) inputs))
+                (def in-to-replace (filter #(= (distance-from-ore dict (key %)) d) inputs))
+                (recur (reduce
+                    (fn [acc x] (merge-with + acc (inputs-for (key x) (val x))))
+                    (into {} in-remaining)
+                    in-to-replace))))))
+
+(println (reduce-to-ore init-state :FUEL 1))
+(test/is (= 741927 ((reduce-to-ore init-state :FUEL 1) :ORE)))
